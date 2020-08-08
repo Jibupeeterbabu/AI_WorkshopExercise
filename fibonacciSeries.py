a0ef83c51dbafbb9f141bf9e01c02d9bd48a7905
Jibupeeterbabu <69351054+Jibupeeterbabu@users.noreply.github.com>
2020-08-08 20:13:19 +0530

    Update fibonacciSeries.py

--- fibonacciSeries.py
+++ fibonacciSeries.py
@@ -1,6 +1,6 @@
 '''
 python program for printing fibonacci series
-sanmple :
+sample :
   fibonacci series: 0, 1, 1, 2, 3, 5,
 logic:
   Nth number = (n-1)th + (n-2)th
@@ -13,15 +13,15 @@ secondNumber = 1
 n = int(input("How many numbers have to be displayed: "))
 
 if(n == 2):
-    print(fistNumber, secondNumber, sep=' ', end=' ' )
+    print(firstNumber, secondNumber, sep=' ', end=' ' )
 elif n == 1:
     print(fistNumber)
 elif n < 0:
     print("Enter a positive number!!")
 else:
-    print(fistNumber, secondNumber, sep=' ', end=' ' )
+    print(firstNumber, secondNumber, sep=' ', end=' ' )
     for i in range(2, n):
-        nextNumber = fistNumber + secondNumber
+        nextNumber = firstNumber + secondNumber
         print(nextNumber, end=' ')
-        fistNumber = secondNumber
+        firstNumber = secondNumber
         secondNumber = nextNumber
